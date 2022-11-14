418b2f20cbdeda1f2d0bbdc0e69d8ae10a254154
Syed Akif Hussain Zaidi <116806308+AkifZaidi@users.noreply.github.com>
2022-11-14 17:05:21 +0500

    assignment assignment
    assignment assignment

---
+++ app.js
@@ -0,0 +1,157 @@
+alert("Hello! I am a alert box ")
+
+alert("Hello World  \n  Happy coding ")
+
+
+alert("alert squences")
+alert("Hello  \n  I am a student")
+
+alert("Developer console")
+
+// Chapter ALERT is complete;
+
+var username;
+var myName = "Syed akif hussain zaidi"
+
+var course = "Hello iam learn javascript"
+
+var message = "Hello World"
+
+alert(message)
+
+alert(myName)
+
+var myAge = "17 year old"
+alert(myAge)
+
+var courseName = "Certified Mobile Application  Development "
+
+
+var pizza = 'PIZZA \n PIZZ \n PIZ \n PI \n P'
+alert(pizza)
+
+
+var email = "zasif2175@gmail.com"
+alert("This is my gmail " + " " + email)
+
+
+var book = "A smater way to learn javascript"
+alert("I am trying to learn from tha book " +  " " +  book)
+
+document.write("YAH! I can Write HTML Content thruogh javascript")
+
+var icon = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(icon)
+
+// chapter  VARIABLE AND STRING is comptele; 
+
+var birth = "17 year old"
+alert(birth)
+
+var visited = "You have visited this site site 5 times"
+alert(visited)
+
+var my_dob = 2005
+document.write(" My birth year is " + my_dob)
+
+var visitorName = "Syed Akif Hussain Zaidi"
+var productName = "T,shirts"
+var total =  "2 T,shirts"
+document.write(" "+visitorName + " ordered" + ' ' + total + "(s) on XYZ Clothing store")
+
+// chapter VARIABLE AND NUMBER is complete;
+
+var  science = "subject"
+var comerce = "subject"
+var cemistry = "subject"
+
+var  legalOne = "This is legal a variable"
+var  $legalTwo = "This is legal a variable"
+var legal_Three = "This is legal a variable"
+var  legal = "This is legal a variable"
+var  legalFour = "This is  legal a veriable"
+
+// var  legal\One = "This is legal a variable"
+// var  @legalTwo = "This is legal a variable"
+// var legal-Three = "This is legal a variable"
+// var  1legal = "This is legal a variable"
+// var  legalFour! = "This is  legal a veriable"
+ 
+document.write("<h1>Rules for naming JS Variables</h1>")
+
+
+var abcd = "numbers";
+var $my = "$my_1stvariable";
+document.write("  Variables names can only cotain ," + abcd + ",$ and_ . For example :" + $my) 
+
+var word  =  "letter";
+var sign = "$"
+var line = "_"
+document.write("   Variable must begin with a" + word + "," + sign + "or" + line + ". For example :$name_name or name")
+
+var letter = "sensitive"
+document.write("  Variable names are case " + letter)
+
+
+var key = "Keywords"
+document.write("  Variable names should not be JS " + key)
+// chapter VARIABLE NAMES: LEGAL & ILLEGAL ia complete;
+
+var ak = 5 + 3
+document.write(ak)
+var fi = 5 - 3 
+document.write(fi)
+var huss = 5 * 3
+document.write(huss)
+var ain = 5 / 3
+document.write(ain)
+
+var a = 1+1+1+1+1+7
+document.write(++a)
+var i;
+document.write(--a)
+
+var price = 600 * 5;
+document.write(" Total cost to buy tickets to a  movie is "+price+"PKR");
+
+var tableOf4 = 4
+for(var i=1 ; i<=10 ; i++){
+ var result = i*tableOf4
+    document.write(`${tableOf4} * ${i} = ${result}<br>`)
+}
+
+
+var firstitemPrice = 650
+var quantityoffirstValue = 3
+var secounditemPrice = 100
+var quantityofsecoundValue = 7
+var shipingCharges = 100
+ 
+document.write("firstitemPrice = 650<br>" + "quantityoffirstValue = 3<br>" +"secounditemPrice = 100<br>" + "quantityofsecoundValue = 7<br>" + "shipingChargses = 100<br>")
+
+var firstMulpication = firstitemPrice * quantityoffirstValue;
+var secoundMutipication = secounditemPrice * quantityofsecoundValue
+var  totalMultipication =  firstMulpication  + secoundMutipication + shipingCharges
+console.log(totalMultipication , secoundMutipication , firstMulpication)
+document.write("Total coast of yourb order is " + totalMultipication)
+
+
+document.write("<h1>mark Sheet</h1>")
+
+var marks = 980;
+var obtain = 804;
+var percentage = marks / obtain * 100;
+document.write("marks = 980<br>"  + "obtain = 804<br>" + percentage);
+
+var number3 = 2
+var number4 = number3 + 5 * 10 / 2
+console.log(number4)
+
+
+var currentYear = 2022
+var birthYear = 2005
+var year = currentYear - birthYear;
+console.log(year)
+
+
+
